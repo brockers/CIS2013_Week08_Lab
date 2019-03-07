73724045749f4516c4909400c5b7421563b34bdb
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-03-06 20:17:54 -0600

    Initial commit

---
+++ Dice.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+class Dice {
+	
+	private:
+		int sides = 6;
+		
+	public:	
+		void setNumberOfSides(int s){
+			if(s <= 100){
+				sides = s;
+			}
+		}
+		
+		int roll(){
+			srand(time(NULL));
+			return rand() % sides + 1;
+		}
+};
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include "Dice.cpp"
+
+using namespace std;
+
+class Cat {
+	public:
+	
+	int size = 22;
+	string breed = "house";
+
+	void roar(){
+		// int size = 45;
+		cout << "The " << size << " inch " << breed;
+		cout << " cat says... Roar!!" << endl;
+	}
+};
+
+int main(){
+	
+	Cat tiger;
+	Cat house;
+
+	cout << "We have a cat" << endl;
+	tiger.size = 45;
+	tiger.breed = "tiger";
+	tiger.roar();
+	
+	cout << "Cat 2 is created" << endl;
+	house.roar();
+	
+	cout << "Create a 8 sided dice" << endl;
+	Dice d6;
+	Dice d8;
+	d8.setNumberOfSides(8);
+	cout << "Roll the dice" << endl;
+	cout << "   the 6 sided dice rolls a " << d6.roll() << endl;
+	d8.setNumberOfSides(1000);
+	cout << "   the 8 sided dice rolls a " << d8.roll() << endl;
+
+	return 0;
+}
